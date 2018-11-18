d8772f04675b28dea32407f35cf32f8ed32ce0f8
Thomas Kleist <thomas@kleist-it.com>
2018-11-18 18:53:32 +0100

    mapping work

--- dlu-persistence-api/daos/StamBladsDao.cs
+++ dlu-persistence-api/daos/StamBladsDao.cs
@@ -16,12 +16,21 @@ namespace dlu_persistence_api.daos
             di = new DiMPdotNetEntities();
         }
 
-        public StamData GetStamDataById(int bladId)
-        {
+        public StamData GetStamDataById(int bladId)       {
            
 
             var res =  di.tblBladStamdatas.Where(r => r.BladID == bladId).FirstOrDefault();
             return dlu_persistence_api.mapper.AutoMaperUtil.ConvertFromTblStamBladEntity(res);
+        }
+
+        public List<StamData> GetStamBladByName(String name, String order, Boolean asc)
+        {
+            var resultat = from m in di.tblBladStamdatas.Where(d => d.Navn.Contains(name)).OrderBy(s => s.Navn).ToList<tblBladStamdata>() select m;
+
+           return 
+
+            
+
         }
 
 
--- dlu-persistence-api/mapper/AutoMaperUtil.cs
+++ dlu-persistence-api/mapper/AutoMaperUtil.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using dlu_persistence_api.models;
+using System.Collections.Generic;
 namespace dlu_persistence_api.mapper
 {
     public class AutoMaperUtil
@@ -12,9 +8,21 @@ namespace dlu_persistence_api.mapper
         public static StamData ConvertFromTblStamBladEntity(tblBladStamdata  tblBladStamdata) {
             var config = new MapperConfiguration(cfg => {
                 cfg.AllowNullCollections = true;
-                cfg.CreateMap<tblBladStamdata, StamData>();
-                
-               cfg.CreateMap<tblDage[],  ICollection<Dage> >(tblBladStamdata.tblDage);
+                cfg.CreateMap<tblBladStamdata, StamData>();                
+               cfg.CreateMap<tblDage, Dage>();
+                cfg.CreateMap<tblDage, Dage>();
+                cfg.CreateMap<tblDage, Dage1>();
+                cfg.CreateMap<tblDelOmråde, DelOmraade>();
+                cfg.CreateMap<tblGeoKode, GeoKode>();
+                cfg.CreateMap<tblHovedGruppe, HovedGruppe>();
+                cfg.CreateMap<tblPostNr, PostNr>();
+                cfg.CreateMap<tblRegion, Region>();
+                cfg.CreateMap<List<tblMedieplanLinjer>, List<MedieplanLinjers>>();
+                cfg.CreateMap<List<tblPriser>, List<Prisers>>();
+                cfg.CreateMap<List<tblPrislisterPrBladPrÅr>, List<PrislisterPrBladÅr>>();
+                cfg.CreateMap<List<tblPrislisterPrBladPrÅr>, List<PrislisterPrBladÅr>>();              
+
+               
             });
           
             var mapper = config.CreateMapper();
@@ -23,7 +31,12 @@ namespace dlu_persistence_api.mapper
                 }
 
 
-         private  ICollection<tblDage>  createDage(tblDage  dage)
+         private static  ICollection<Dage>  createDage(tblDage dage)
+        {
+            var res = new List<Dage>() ;
+            res.Add(new Dage() { });
+            return res;
+        }
          
     
 
@@ -31,3 +44,12 @@ namespace dlu_persistence_api.mapper
     }
 
 }
+
+/*
+ *   
+       
+      
+      
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<tblPrislisterPrBladPrÅr> tblPrislisterPrBladPrÅr { get; set; }
+*/
