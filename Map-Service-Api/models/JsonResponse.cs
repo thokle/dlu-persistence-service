f87e43e28d94e31f538b15c35b29004c1a1f98a1
thomaskleist <thomas@kleist-it.com>
2020-12-06 21:22:46 +0100

    added jsons files

---
+++ Map-Service-Api/models/JsonResponse.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Map_Service_Api.services
+{
+
+
+    public class Properties
+    {
+        public string name { get; set; }
+    }
+
+    public class Crs
+    {
+        public string type { get; set; }
+        public Properties properties { get; set; }
+    }
+
+    public class Properties2
+    {
+        public string POSTNR_TXT { get; set; }
+        public string POSTBYNAVN { get; set; }
+    }
+
+    public class Geometry
+    {
+        public string type { get; set; }
+        public List<List<List<double>>> coordinates { get; set; }
+    }
+
+    public class Feature
+    {
+        public string type { get; set; }
+        public Properties2 properties { get; set; }
+        public Geometry geometry { get; set; }
+    }
+
+    public class Root
+    {
+        public string type { get; set; }
+        public Crs crs { get; set; }
+        public List<Feature> features { get; set; }
+    }
+
+
+
+}
